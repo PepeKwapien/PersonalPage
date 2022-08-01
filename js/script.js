e3a6261f4d2d010bde3d3f9d83832a1ca69ccba7
PepeKwapien <pppkwapien@gmail.com>
2022-08-01 23:11:44 +0200

    adding change of my name animation

---
+++ js/script.js
@@ -0,0 +1,50 @@
+var myName = document.getElementById("myName");
+console.log(myName);
+
+let names = [
+  ["e", "p", "e"],
+  ["a", "w", "e", "l"],
+];
+
+async function waitUntil(condition, action, intervalTime) {
+  return await new Promise((resolve) => {
+    const interval = setInterval(() => {
+      if (condition()) {
+        resolve();
+        clearInterval(interval);
+      } else {
+        action();
+      }
+    }, intervalTime);
+  });
+}
+
+var whichName = 0;
+var intervalTime = 200;
+var lap = 0;
+
+async function removeAndChangeName() {
+  await waitUntil(
+    () => myName.innerHTML.length <= 1,
+    () => {
+      myName.innerHTML = myName.innerHTML.slice(0, -1);
+    },
+    intervalTime
+  );
+
+  var whichLetter = 0;
+
+  await waitUntil(
+    () => myName.innerHTML.length > names[whichName].length,
+    () => {
+      myName.innerHTML += names[whichName][whichLetter];
+      whichLetter++;
+    },
+    2 * intervalTime
+  );
+
+  whichName = ++whichName % 2;
+}
+
+setTimeout(removeAndChangeName, 2500);
+setInterval(removeAndChangeName, 5000);
